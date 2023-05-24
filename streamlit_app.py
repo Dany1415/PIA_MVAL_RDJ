812529ec69065ff962d4cb7bf551e8540a734931
Daniela Ruiz Jiménez <78112067+Dany1415@users.noreply.github.com>
2023-05-23 22:32:09 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,16 @@
 import streamlit as st
+import pandas as pd
 
 # Título de la página
-st.title("Carga de imágenes en Streamlit")
+st.title("Carga de archivo CSV en Streamlit")
 
-# Componente para cargar la imagen
-uploaded_file = st.file_uploader("Selecciona una imagen", type=["jpg", "jpeg", "png"])
+# Componente para cargar el archivo CSV
+uploaded_file = st.file_uploader("Selecciona un archivo CSV", type=["csv"])
 
 # Verificar si se cargó un archivo
 if uploaded_file is not None:
-    # Mostrar la imagen cargada
-    st.image(uploaded_file, caption="Imagen cargada", use_column_width=True)
+    # Leer el archivo CSV y cargarlo en un DataFrame
+    df = pd.read_csv(uploaded_file)
+
+    # Mostrar el DataFrame
+    st.write(df)
