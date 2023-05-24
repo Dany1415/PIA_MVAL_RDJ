8c80a24121dfa1a62849b3b5a4f49595275cf3a5
Daniela Ruiz Jiménez <78112067+Dany1415@users.noreply.github.com>
2023-05-23 23:16:48 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,12 @@
 import streamlit as st
-import pandas as pd
-import matplotlib.pyplot as plt 
 
 # Título de la página
-st.title("Carga de archivo CSV en Streamlit")
+st.title("Carga de imágenes en Streamlit")
 
-# Componente para cargar el archivo CSV
-uploaded_file = st.file_uploader("Selecciona un archivo CSV", type=["csv"])
+# Componente para cargar la imagen
+uploaded_file = st.file_uploader("Selecciona una imagen", type=["jpg", "jpeg", "png"])
 
 # Verificar si se cargó un archivo
 if uploaded_file is not None:
-    # Leer el archivo CSV y cargarlo en un DataFrame
-    df = pd.read_csv(uploaded_file)
-
-    # Mostrar el DataFrame
-    st.write(df)
-    
-    Transacciones = df.values.reshape(-1).tolist() #-1 significa 'dimensión no conocida'
-    Lista = pd.DataFrame(Transacciones)
-    Lista['Frecuencia'] = 1
-    Lista = Lista.groupby(by=[0], as_index=False).count().sort_values(by=['Frecuencia'], ascending=True) #Conteo
-    Lista['Porcentaje'] = (Lista['Frecuencia'] / Lista['Frecuencia'].sum()) #Porcentaje
-    Lista = Lista.rename(columns={0 : 'Item'})
-  
-    plt.figure(figsize=(16,20), dpi=300)
-    plt.ylabel('Item')
-    plt.xlabel('Frecuencia')
-    plt.barh(Lista['Item'], width=Lista['Frecuencia'], color='red')
-    grafica_path = 'static/img/grafica.png'
-    image = plt.savefig(grafica_path)
-    st.image(image, caption="Carga pofavo", use_column_width=True)
+    # Mostrar la imagen cargada
+    st.image(uploaded_file, caption="Imagen cargada", use_column_width=True)
