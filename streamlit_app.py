c2febc57b7a566d2c7709779317ca6fdbb1a0f1f
Daniela Ruiz Jiménez <78112067+Dany1415@users.noreply.github.com>
2023-05-23 22:18:07 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd 
 import numpy as np                  # Para crear vectores y matrices n dimensionales
 #import matplotlib.pyplot as plt     # Para la generación de gráficas a partir de los datos
-from apyori import apriori
+#from apyori import apriori
 
 image = Image.open('IA_salud.png')
 st.image(image, use_column_width = True)
